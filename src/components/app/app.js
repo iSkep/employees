9c48c88c71bf84736086e5ab09e38fde4a53073d
Skep <madskep@gmail.com>
2022-07-21 19:14:52 +0300

    Add search functionality

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
                 {name: 'John C.', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Andy L.', salary: 3000, increase: true, rise: false, id: 2},
                 {name: 'Jack B.', salary: 5000, increase: false, rise: false, id: 3}
-            ]
+            ],
+            term: ''
         }
         this.maxId = 4;
     }
@@ -81,9 +82,25 @@ class App extends Component {
         }))
     }
 
+    searchEmployee = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1
+        })
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
     render () {
+        const {data, term} = this.state;
         const employeesCount = this.state.data.length;
         const employeesIncreasedCount = this.state.data.filter(item => item.increase).length;
+        const visibleData = this.searchEmployee(data, term);
 
         return (
             <div className="app">
@@ -93,12 +110,14 @@ class App extends Component {
                 />
     
                 <div className="search-panel">
-                    <SearchPanel />
+                    <SearchPanel 
+                        onUpdateSearch={this.onUpdateSearch}
+                    />
                     <AppFilter />
                 </div>
                 
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleIncrease={this.onToggleIncrease}
                     onToggleRise={this.onToggleRise}
